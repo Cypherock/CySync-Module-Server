268e4a1aed5c6f2ec6bf011804abedea72a6a57a
Harish <harish@cypherock.com>
2022-05-17 12:23:31 +0530

    fix formatting of response

--- src/resources/bitcoin/transaction.ts
+++ src/resources/bitcoin/transaction.ts
@@ -104,12 +104,12 @@ export function getOpenTxnLink(params: {
 
 export async function getTxnHex(params: { hash: string; coinType: string }) {
   const res = await http
-    .post(`v2/${baseURL}/hex`, params, {
+    .post(`v2${baseURL}/hex`, params, {
       key: `BTHX-${params.coinType}`,
       ttl: 60,
       isRefresh: false
     })
     .request();
 
-  return res.data;
+  return res.data.data;
 }
