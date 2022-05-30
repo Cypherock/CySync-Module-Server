54e2035b1e195f789407da6abda8ebaa6fd698a3
HarishRocks <102788106+HarishRocks@users.noreply.github.com>
2022-05-31 01:25:10 +0530

    Csi 1017 server cleanup 20 (#7)
    * remove blockcypher fetchAddress
    
    * remove blockcypher addressHistory
    
    * remove blockcypher fetchBalance
    
    * remove blockcypher fullHistory
    
    * remove blockcypher history
    
    * remove blockcypher fetchUtxos
    
    * update version
    
    * remove add address and add wallet
    
    * remove getReceive Txn
    
    * 📦 NEW: Updated beta version
    
    Co-authored-by: Md Irshad Ansari <irshad@cypherock.com>

--- src/resources/bitcoin/transaction.ts
+++ src/resources/bitcoin/transaction.ts
@@ -4,74 +4,11 @@ import http from '../../utils/http';
 
 const baseURL = '/transaction';
 
-export function getHistory(
-  params: { walletName: string; coinType: string; limit?: number },
-  isRefresh?: boolean
-) {
-  let url = `${baseURL}/history`;
-
-  if (isRefresh) {
-    url += '?isRefresh=true';
-  }
-
-  return http.post(url, params, {
-    key: `BTH-${params.coinType}-${params.walletName}-${params.limit}`,
-    ttl: 60,
-    isRefresh
-  });
-}
-
-export function getFullHistory(
-  params: {
-    walletName: string;
-    coinType: string;
-    before?: number;
-    after?: number;
-  },
-  isRefresh?: boolean
-) {
-  let url = `${baseURL}/full-history`;
-
-  if (isRefresh) {
-    url += '?isRefresh=true';
-  }
-
-  return http.post(url, params, {
-    key: `BFTH-${params.coinType}-${params.walletName}-${params.before}-${params.after}`,
-    ttl: 60,
-    isRefresh
-  });
-}
-
-export function getAddressHistory(
-  params: {
-    address: string;
-    coinType: string;
-  },
-  isRefresh?: boolean
-) {
-  let url = `${baseURL}/address-history`;
-
-  if (isRefresh) {
-    url += '?isRefresh=true';
-  }
-
-  return http.post(url, params, {
-    key: `BTAH-${params.coinType}`,
-    ttl: 60,
-    isRefresh
-  });
-}
-
 export function broadcastTxn(params: {
   transaction: string;
   coinType: string;
 }) {
   return http.post(`${baseURL}/broadcast`, params);
-}
-
-export function getReceivePath() {
-  return `${config.BASE_URL}${baseURL}/receive`;
 }
 
 export function getFees(params: { coinType: string }, isRefresh?: boolean) {
@@ -86,10 +23,6 @@ export function getFees(params: { coinType: string }, isRefresh?: boolean) {
     ttl: 60,
     isRefresh
   });
-}
-
-export function setHook(params: { receiveAddress: string; coinType: string }) {
-  return http.post(`${baseURL}/set-hook`, params);
 }
 
 export function getOpenTxnLink(params: {
